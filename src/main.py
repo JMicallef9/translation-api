2320a9773849cc2d5f65cbd7f7647484e4455993
Joseph Micallef <micallefje@gmail.com>
2024-12-11 10:51:18 +0000

    add more tests for post request

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from deep_translator import GoogleTranslator
+import langdetect
+from datetime import datetime
+
+
+langdetect.DetectorFactory.seed = 0
 
 app = FastAPI()
 
@@ -8,8 +13,18 @@ class TranslationRequest(BaseModel):
     text: str
     target_lang: str
 
+langs_dict = GoogleTranslator().get_supported_languages(as_dict=True)
+
 @app.post("/translate/", status_code=201)
 def translate_text(request: TranslationRequest):
+    if request.target_lang not in langs_dict.values():
+        raise HTTPException(status_code=400, detail=f"Invalid request. Language code {request.target_lang} not supported.")
     translator = GoogleTranslator(target=request.target_lang)
     translated_text = translator.translate(request.text)
-    return {"original_text": request.text, "translated_text": translated_text}
+    return {
+        "original_text": request.text, 
+        "original_lang": langdetect.detect(request.text), 
+        "translated_text": translated_text,
+        "output_lang": request.target_lang,
+        "timestamp": datetime.now().isoformat()
+        }
--- test/test_main.py
+++ test/test_main.py
@@ -1,10 +1,42 @@
 from fastapi.testclient import TestClient
 from src.main import app
+import pytest
+from unittest.mock import patch, MagicMock
+
+@pytest.fixture()
+def test_client():
+	client = TestClient(app)
+	return client
+
+@pytest.fixture()
+def de_translation_request(test_client):
+	response = test_client.post("/translate/", json={"text": "Hello world", "target_lang": "de"})
+	return response
+
+@pytest.fixture()
+def datetime_mock():
+	with patch('src.main.datetime') as mock_dt:
+		mock_now = MagicMock()
+		mock_now.isoformat.return_value = 'mock_timestamp'
+		mock_dt.now.return_value = mock_now
+		yield mock_dt
 
 class TestPostTranslate:
-	def test_returns_201_status_code(self):
-		client = TestClient(app)
-		response = client.post("/translate/", json={"text": "Hello world", "target_lang": "de"})
-		assert response.status_code == 201
-		assert response.json()['original_text'] == 'Hello world'
-		assert response.json()['translated_text'] == 'Hallo Welt'
+	def test_returns_201_status_code(self, de_translation_request):
+		assert de_translation_request.status_code == 201
+	
+	def test_response_includes_original_and_translated_text(self, de_translation_request):
+		assert de_translation_request.json()['original_text'] == 'Hello world'
+		assert de_translation_request.json()['translated_text'] == 'Hallo Welt'
+
+	def test_response_includes_original_and_target_langs(self, de_translation_request):
+		assert de_translation_request.json()['original_lang'] == 'en'
+		assert de_translation_request.json()['output_lang'] == 'de'
+
+	def test_response_includes_timestamp(self, datetime_mock, test_client):
+		response = test_client.post("/translate/", json={"text": "Hello world", "target_lang": "de"})
+		assert response.json()['timestamp'] == 'mock_timestamp'
+	
+	def test_invalid_target_lang_returns_error_message(self):
+		response = test_client.post("/translate/", json={"text": "Hello world", "target_lang": "de"})
+
