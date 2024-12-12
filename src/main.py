c0d592237b5abd2796b414beebe3752de21b5dda
Joseph Micallef <micallefje@gmail.com>
2024-12-12 12:02:32 +0000

    add further error handling

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from deep_translator import GoogleTranslator
 import langdetect
 from datetime import datetime
+from typing import Optional
 
 
 langdetect.DetectorFactory.seed = 0
@@ -12,21 +13,34 @@ app = FastAPI()
 class TranslationRequest(BaseModel):
     text: str
     target_lang: str
+    input_lang: Optional[str] = None
 
 langs_dict = GoogleTranslator().get_supported_languages(as_dict=True)
 
 @app.post("/translate/", status_code=201)
 def translate_text(request: TranslationRequest):
-    if request.target_lang not in langs_dict.values():
-        raise HTTPException(status_code=422, detail=f"Invalid request. Language code {request.target_lang} is not recognised.")
-    translator = GoogleTranslator(target=request.target_lang)
+    if request.target_lang.lower() not in [lang.lower() for lang in langs_dict.values()]:
+        raise HTTPException(status_code=422, detail=f"Invalid language code: {request.target_lang}. Please check the list of supported languages.")
+
+    if not request.text:
+        raise HTTPException(status_code=422, detail='Empty input provided. Please try again.')
+
+    detected_lang = langdetect.detect(request.text)
+    if request.input_lang:
+        if request.input_lang != detected_lang:
+            raise HTTPException(status_code=422, detail=f'Mismatch detected. Input language specified as "{request.input_lang}", but language detected as "{detected_lang}". Please try again.')
+
+    translator = GoogleTranslator(source=detected_lang, target=request.target_lang.lower())
     translated_text = translator.translate(request.text)
+
+
     if request.text == translated_text:
         raise HTTPException(status_code=422, detail='Translation error. Inputted text was not recognised. Please try again.')
+
     return {
         "original_text": request.text, 
-        "original_lang": langdetect.detect(request.text), 
+        "original_lang": detected_lang, 
         "translated_text": translated_text,
-        "output_lang": request.target_lang,
+        "output_lang": request.target_lang.lower(),
         "timestamp": datetime.now().isoformat()
         }
--- test/test_main.py
+++ test/test_main.py
@@ -40,10 +40,25 @@ class TestPostTranslate:
 	def test_invalid_target_lang_returns_error_message(self, test_client):
 		response = test_client.post("/translate/", json={"text": "Hello world", "target_lang": "qq"})
 		assert response.status_code == 422
-		assert response.json() == {"detail": "Invalid request. Language code qq is not recognised."}
+		assert response.json() == {"detail": "Invalid language code: qq. Please check the list of supported languages."}
 	
 	def test_recognises_invalid_request_text(self, test_client):
 		response = test_client.post("/translate/", json={"text": "zjxhakgdhgadshg", "target_lang": "de"})
 		assert response.status_code == 422
 		assert response.json() == {'detail': 'Translation error. Inputted text was not recognised. Please try again.'}
 
+	def test_empty_input_returns_error_message(self, test_client):
+		response = test_client.post("/translate/", json={"text": "", "target_lang": "de"})
+		assert response.status_code == 422
+		assert response.json() == {'detail': 'Empty input provided. Please try again.'}
+	
+	def test_language_code_detection_is_case_insensitive(self, test_client):
+		response = test_client.post("/translate/", json={"text": "Hello world", "target_lang": "DE"})
+		assert response.status_code == 201
+		assert response.json()['output_lang'] == 'de'
+
+	def test_error_message_returned_if_user_specified_input_lang_does_not_match_detected_lang(self, test_client):
+		payload = {"text": "Hello world", "target_lang": "DE", "input_lang": "lt"}
+		response = test_client.post("/translate/", json=payload)
+		assert response.status_code == 422
+		assert response.json()['detail'] == 'Mismatch detected. Input language specified as "lt", but language detected as "en". Please try again.'
