e56f0d78058b6f592f2f120a56d6b639452b0630
Joseph Micallef <micallefje@gmail.com>
2025-01-06 12:46:36 +0000

    update test parameters

--- test/test_s3_utils.py
+++ test/test_s3_utils.py
@@ -26,13 +26,13 @@ def dummy_request():
 
 class TestSaveToS3:
     def test_object_saved_to_s3_bucket(self, s3_mock, dummy_request):
-        save_to_s3(dummy_request, s3_mock, 'test_bucket')
+        save_to_s3(dummy_request, 'test_bucket')
 
         object_list = s3_mock.list_objects_v2(Bucket='test_bucket')
         assert object_list['Contents'][0]['Key'] == '2015-01-27T05:57:31.399861+00:00'
 
     def test_object_body_contains_expected_data(self, s3_mock, dummy_request):
-        save_to_s3(dummy_request, s3_mock, 'test_bucket')
+        save_to_s3(dummy_request, 'test_bucket')
 
         result = s3_mock.get_object(
             Bucket='test_bucket',
